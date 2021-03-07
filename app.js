4a58e6f76b531bf618de7411711fc3ec7143ae6a
Majedah Hatamleh <MAJIDAHATAMLEH@OUTLOOK.COM>
2021-03-07 12:42:03 +0200

    problem4

--- app.js
+++ app.js
@@ -71,8 +71,6 @@ var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
 
-}
-
 // Here is the test for sumArray(); uncomment it to run it
 
 // testSumArray(testArray);
